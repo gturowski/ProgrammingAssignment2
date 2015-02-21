a8b1b3090461a2206ee5215ae7ecfce9e1af4db2
Grzegorz <grzetur@gmail.com>
2015-02-21 15:41:00 -0800

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
-The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to:
+# The first function, makeCacheMatrix creates a special "vector",
+# which is really a list containing a function to:
 # 1. set the value of the matrix
 # 2. get the value of the matrix
 # 3. set the value of inverse of the matrix
